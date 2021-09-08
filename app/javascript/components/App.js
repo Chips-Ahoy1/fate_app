31f58c52728008d0ac699e2a127c2a053d378720
Trzy426 <charleswrogersiii@gmail.com>
2021-09-08 16:03:50 -0700

    refactored to include class componet for page calls

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -7,14 +7,21 @@ import {
   Switch
 } from "react-router-dom"
 import { Nav, NavItem } from "reactstrap" 
+import Header from "./components/Header"
 class App extends React.Component {
   render() {
+
     return (
-      <Router>
+        <Router>
+          <Header {...this.props}/>
         <Switch>
-           {/* <Route exact path="/" component={ Home } /> */}
-            {/* <Route path="/about" component={ AboutUs } />
-           <Route path="/learn" component={ LearnMore } /> */}
+           {/* <Route exact path="/" component={ Home } /> 
+          <Route path="/about" component={ AboutUs } />
+           <Route path="/learn" component={ LearnMore } />
+           <Route path="/learn" component={ LearnMore } />
+           <Route path="/learn" component={ LearnMore } />
+           <Route path="/learn" component={ LearnMore } />
+            */}
         </Switch>
       </Router>
     );
