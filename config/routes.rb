e5e3b6f284a0f2d5af8dc5e76550d53aa059cd8e
christophermackey <c.mackey@me.com>
2021-09-08 14:31:11 -0700

    adds devise and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 
 Rails.application.routes.draw do
+  resources :events
+  devise_for :users
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'
 end
---
+++ db/migrate/20210908211051_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.text :image_url
+      t.string :category
+      t.boolean :is_public
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
