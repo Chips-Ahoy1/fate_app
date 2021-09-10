41904a91e55e530f282a4c68b2ac329585c888e5
galadoe <kgaladoe@gmail.com>
2021-09-10 15:41:06 -0700

    created the create and working on index functionality

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -6,22 +6,67 @@ import {
   Route,
   Switch
 } from "react-router-dom"
-import { Nav, NavItem } from "reactstrap" 
+import { Nav, NavItem } from "reactstrap"
 import Header from "./components/Header"
+import FateNew from "./pages/FateNew"
+import FateIndex from "./pages/FateIndex"
+import Home from "./pages/Home"
+
+
 class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount() {
+    this.eventIndex()
+  }
+
+  eventIndex = () => {
+    fetch("http://localhost:3000/fate")
+      .then(eventsArray => this.setState({ events: eventsArray }))
+      .catch(errors => console.log("Event read errors: ", errors))
+  }
+
+  createNewEvent = (newEvent) => {
+      console.log(this.props.logged_in)
+      console.log(newEvent)
+      fetch("/fate", {
+      body: JSON.stringify(newEvent),
+      headers: {'Content-Type': 'application/json'},
+      method: "POST"
+    })
+    .then(response => {
+      console.log("response check")
+      if (response.status === 422){
+        alert("Please try again")
+      }
+      return response.json
+
+      // window.location = "/apartmentindex"
+    })
+    .then(payload => {
+      console.log("payload")
+      return this.fateIndex()
+    })
+    .catch(err => {
+      console.log("createnewerror: ", err)
+    })
+  }
+
   render() {
 
     return (
         <Router>
           <Header {...this.props}/>
+          <Route exact path="/" component={ Home } />
+          <Route path="/fateindex" render={ (props) => <FateIndex events={ this.state.events }/> } />
+          <Route path="/fatenew" render={ (props) => <FateNew createEvent={this.createEvent} /> } />
         <Switch>
-           {/* <Route exact path="/" component={ Home } /> 
-          <Route path="/about" component={ AboutUs } />
-           <Route path="/learn" component={ LearnMore } />
-           <Route path="/learn" component={ LearnMore } />
-           <Route path="/learn" component={ LearnMore } />
-           <Route path="/learn" component={ LearnMore } />
-            */}
         </Switch>
       </Router>
     );
--- app/javascript/components/pages/FateIndex.js
+++ app/javascript/components/pages/FateIndex.js
@@ -1,11 +1,28 @@
 import React,{Component} from 'react'
 
 class FateIndex extends Component{
+  //this page still needs work 
     render() {
 
         return(
-
-            <h1>This is the FateIndex page</h1>
+            <>
+            <div>
+        <h2>This is Fate Index Page</h2>
+        <br />
+        <Col sm="6">
+          {this.props.events && this.props.events.map(events => {
+            return(
+              <Card body className="text-center" key={ apartment.id }>
+                <img src=event.image_url/>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    { `${event.description} `}
+                  </CardSubtitle>
+            </Card>
+            )
+          })}
+        </Col>
+      </div>
+            </>
 
         )
     }
