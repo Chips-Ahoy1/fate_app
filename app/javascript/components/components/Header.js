3a9ec0d0a1d9b74639b237a4a93428a1bbf807c8
Bpompa95 <brianpompa02@gmail.com>
2021-09-10 19:54:56 -0700

    fix style a little more

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -12,8 +12,7 @@ class Header extends Component {
     } = this.props
     return (
       <>
-
-        <Nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-100 mb-3">
+         <Nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-100 mb-3">
           <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
               <NavItem>
