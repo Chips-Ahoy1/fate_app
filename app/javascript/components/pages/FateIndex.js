58b4ac1cdd5f989e97ed4a8c89ed2c77f671678c
christophermackey <c.mackey@me.com>
2021-09-15 12:25:49 -0700

    adds index styling

--- app/javascript/components/pages/FateIndex.js
+++ app/javascript/components/pages/FateIndex.js
@@ -3,7 +3,21 @@ import { NavItem, Nav, Col, Card, CardSubtitle } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import Button from '../components/Button'
 
+const CardComponent = ({ event }) => {
+    return (<div className="w-full flex justify-center items-end padding mb-6">
+        <div>
+            <NavLink className="names" to={`/fateshow/${event.id}`}>
+                <img src={event.image_url} alt={event.category} />
+            </NavLink>
+            <div className="bg-purple-300 w-full h-20 flex items-center justify-center">
+                <p>
+                    {event.description}
+                </p>
+            </div>
 
+        </div>
+    </div>)
+}
 class FateIndex extends Component {
     handleClick = () => {
         console.log('hello world');
@@ -15,21 +29,7 @@ class FateIndex extends Component {
                     <h2>This is Fate Index Page Test1</h2>
                     {this.props.events && this.props.events.map((event, i) => {
                         return (
-                            <div>
-                            <div key={i}> 
-                            <img src={event.image_url} alt={event.category} />
-                                <p>
-                                    {event.description}
-                                </p>
-                            </div>
-                            <div>
-                              <Nav>
-                                 <NavItem>
-                                <NavLink className="names" to={`/fateshow/${event.id}`}>View</NavLink>
-                                </NavItem>
-                             </Nav>
-                             </div>
-                             </div>
+                            <CardComponent event={event} i={i} />
                         )
                     })}
                     <Button title="Add an interest" handleClick={this.handleClick} />
