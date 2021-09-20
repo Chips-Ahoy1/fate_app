450054f95535aaa87adc5df341c607f4b16fed17
galadoe <kgaladoe@gmail.com>
2021-09-20 14:18:33 -0700

    about us description added

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -7,25 +7,25 @@ const teamchips = [
     id: "Brian Pompa",
     image_url:
       "https://media.istockphoto.com/photos/dog-jumping-in-autumn-picture-id1280392364?s=612x612",
-    description: "Brian Pompa | Product Manager",
+    description: "Brian Pompa | Product Manager | Manages the conversation around large changes to the app; Ensures requirements are met",
   },
   {
     id: "Galadoe Kulee",
     image_url:
       "https://media.istockphoto.com/photos/dog-jumping-in-autumn-picture-id1280392364?s=612x612",
-    description: "Galadoe Kulee | Project Manager ",
+    description: "Galadoe Kulee | Project Manager | Ensures developers are assigned to the card they are working on, decides",
   },
   {
     id: "Trey Rogers",
     image_url:
       "https://media.istockphoto.com/photos/dog-jumping-in-autumn-picture-id1280392364?s=612x612",
-    description: "Trey Rogers | Tech Lead",
+    description: "Trey Rogers | Tech Lead | Merges code when appropriate; Must have a strong understanding of the technical needs of the project",
   },
   {
     id: "Chris Mackey",
     image_url:
       "https://media.istockphoto.com/photos/dog-jumping-in-autumn-picture-id1280392364?s=612x612",
-    description: "Chris Mackey | Design Lead",
+    description: "Chris Mackey | Design Lead | Navigation and user experience; Color schemes, implementation of branding",
   },
 ];
 class AboutUs extends Component {
